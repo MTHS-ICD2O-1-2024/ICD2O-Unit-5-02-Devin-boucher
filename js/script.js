724aa5a322a57b8c4c52368fa09e1cad6d5db76d
default <devin.boucher@stu.ocsb.ca>
2025-04-22 17:29:20 +0000

    fixed code

--- js/script.js
+++ js/script.js
@@ -11,13 +11,13 @@ function checkNumber () {
   // declaring variable
   let number
   // Get input values
-  const isPositive = document.getElementById("option-positive").checked
+  const isPositive = document.getElementById('option-positive').checked
 
-if (isPositive) {
+  if (isPositive) {
     number = Math.floor(Math.random() * 6) + 1
   } else {
     number = -1 * (Math.floor(Math.random() * 6) + 1)
   }
 
-  document.getElementById("result").innerHTML = `<p>Your number is: <strong>${number}</strong></p>`;
+  document.getElementById('result').innerHTML = `<p>Your number is: <strong>${number}</strong></p>`
 }
