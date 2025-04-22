2281f161430c5824f531fbd959d61d7d168e796b
default <devin.boucher@stu.ocsb.ca>
2025-04-22 17:14:54 +0000

    made js code

--- js/script.js
+++ js/script.js
@@ -5,27 +5,19 @@
 // This file contains the JS functions for index.html
 
 'use strict'
-const randomNumber = Math.floor(Math.random() * 6) + 1
 
-/**
-  * Checks random number
-  */
 // eslint-disable-next-line no-unused-vars
 function checkNumber () {
+  // declaring variable
+  let number
   // Get input values
-  const optionPositive = parseFloat(document.getElementById('option-positive').value)
+  const isPositive = document.getElementById("option-positive").checked
 
-  // process
-  if (else) (optionPositive === randomNumber) {
-    // output
-    document.getElementById('optionPositive').innerHTML =
-      '<p>You have guessed the correct number</p>'
+if (isPositive) {
+    number = Math.floor(Math.random() * 6) + 1
+  } else {
+    number = -1 * (Math.floor(Math.random() * 6) + 1)
   }
 
-  if (userNumber !== randomNumber) {
-    // output
-    document.getElementById('result').innerHTML =
-      '<p>You have guessed the wrong number' +
-      '<br/>The correct number was: ' + randomNumber + '</p>'
-  }
+  document.getElementById("result").innerHTML = `<p>Your number is: <strong>${number}</strong></p>`;
 }
